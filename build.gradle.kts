79a34c1ed7aa2a148f407edc93cf8a2b418e9fd6
Keith Policano <keith.policano@us.pwc.com>
2020-05-21 08:04:59 -0500

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,17 +17,17 @@ repositories {
 }
 
 dependencies {
-  implementation("org.scala-lang:scala-library:2.13.1")
+  implementation("org.scala-lang:scala-library:2.13.2")
 
   testImplementation("junit:junit:4.13")
-  testImplementation("org.scalatest:scalatest_2.13:3.1.1")
-  testImplementation("org.scalatestplus:junit-4-12_2.13:3.1.1.0")
-  testImplementation("org.scalatestplus:mockito-3-2_2.13:3.1.1.0")
-  testImplementation("org.mockito:mockito-core:3.3.0")
+  testImplementation("org.scalatest:scalatest_2.13:3.1.2")
+  testImplementation("org.scalatestplus:junit-4-12_2.13:3.1.2.0")
+  testImplementation("org.scalatestplus:mockito-3-2_2.13:3.1.2.0")
+  testImplementation("org.mockito:mockito-core:3.3.3")
   testImplementation("org.apache.commons:commons-dbcp2:2.7.0")
 
-  testRuntimeOnly("org.mariadb.jdbc:mariadb-java-client:2.5.4")
-  testRuntimeOnly("org.postgresql:postgresql:42.2.10")
+  testRuntimeOnly("org.mariadb.jdbc:mariadb-java-client:2.6.0")
+  testRuntimeOnly("org.postgresql:postgresql:42.2.12")
   testRuntimeOnly("com.h2database:h2:1.4.200")
 }
 
